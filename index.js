140488084519056a3e0f247c5beaac7d6d671858
Alex Appleget <137813659+alexappleget@users.noreply.github.com>
2023-09-09 15:13:11 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+setInterval(setClock, 1000)
+
+const hourHand = document.getElementById('hour');
+const minuteHand = document.getElementById('minute');
+const secondHand = document.getElementById('second');
+
+function setClock() {
+    const currentDate = new Date();
+    const secondsRatio = currentDate.getSeconds() / 60;
+    const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60;
+    const hoursRatio = (minutesRatio + currentDate.getHours()) / 12;
+    setRotation(secondHand, secondsRatio);
+    setRotation(minuteHand, minutesRatio);
+    setRotation(hourHand, hoursRatio);
+}
+
+function setRotation(element, rotationRatio) {
+    element.style.setProperty('--rotation', rotationRatio * 360);
+}
+
+setClock();
